63e88fb19fa5cd9b55fc06e74cb0f5ce744fc0dc
Husssains3 <hussainasabbir3@gmail.com>
2019-12-08 12:41:34 +0600

    My first programe,js calculator

---
+++ main.js
@@ -0,0 +1,5 @@
+//first programe
+var first = prompt("Enter your first number");//prompt used to take input form user
+var second = prompt("Enter your first second");
+var sum = Number(first) + Number(second);//Number used to make a steing into number
+alert(sum);// alert used to print something in sceen
