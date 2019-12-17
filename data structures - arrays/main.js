0a33f80b1135c1362b5539dbc2db35c17d3c757f
Husssains3 <hussainasabbir3@gmail.com>
2019-12-17 17:36:49 +0600

    Object

---
+++ data structures - arrays/main.js
@@ -0,0 +1,9 @@
+var employee = [
+    ["Sabbir","Dip","Rasel"],
+    ["Alamin","Roni","Bappy"],
+    ["Reza","Joti","Ahsan"]
+];
+
+
+var newList = employee.concat(["hatim","Hobo","Jorin"]);
+console.log(newList);
---
+++ objects/main.js
@@ -0,0 +1,13 @@
+var person = {
+    name: "Sabbir",
+    agr: "26",
+    height: "6",
+
+    shout: function () {
+        console.log("Hello");
+    }
+}
+
+person.ocupation = "Developer";
+console.log(person);
+console.log(person.shout());
