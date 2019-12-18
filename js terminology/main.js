d5ac051b9336a6500b8cf02b649c266ce686381f
Husssains3 <hussainasabbir3@gmail.com>
2019-12-18 18:10:50 +0600

    Complete till jsTerminology 18-dec-19

---
+++ js terminology/main.js
@@ -0,0 +1,32 @@
+//function declaration
+function newFunction(){}
+
+
+//function expression
+var myFunction =  function newFunction(){};
+var myFunction =  function (){};
+
+
+//expretion
+1+3;
+var zero = 0;
+return 1+2 ;
+
+//calling a function
+newFunction();
+newFunction(ami,tumi);
+
+//assign a variable
+var a = true;
+
+
+//function vs method
+function oldFunction(){}
+var clarr= { 
+    anewMethod: function(){}
+}
+
+//calling function vs method
+oldFunction();
+clarr.anewMethod();
+
