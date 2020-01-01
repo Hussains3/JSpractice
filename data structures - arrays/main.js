04c50c60d966f96c236f139dcfd4c733096db62a
Husssains3 <hussainasabbir3@gmail.com>
2020-01-01 18:21:37 +0600

    Creating template

--- data structures - arrays/main.js
+++
@@ -1,9 +0,0 @@
-var employee = [
-    ["Sabbir","Dip","Rasel"],
-    ["Alamin","Roni","Bappy"],
-    ["Reza","Joti","Ahsan"]
-];
-
-
-var newList = employee.concat(["hatim","Hobo","Jorin"]);
-console.log(newList);
